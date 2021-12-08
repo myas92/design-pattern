5f16fb40b8f1b84a4357f04cd4417da3325a90f4
yaser ahmadi <m.y.ahmadi22@gmail.com>
2021-12-08 15:28:49 +0330

    Update index.js

--- builder/index.js
+++ builder/index.js
@@ -2,7 +2,6 @@ const PersonBuilder = require('./person-builder');
 
 const personBuilder = new PersonBuilder()
 
-
 let person = personBuilder.works.at('part').asA('CTO').earning('1200')
   .lives.at('Ahmad Abad').in('Mashhad').withPostCode('3231')
   .build()
